51eed3ebc4b46d79d6b754a17f65764a07bd3e1c
VanezKun <fanes23.pratama@mhs.mdp.ac.id>
2024-05-25 22:22:33 +0800

    refactor: balance body content

--- src/pages/Balance/partials/BalanceBodyContent.tsx
+++ src/pages/Balance/partials/BalanceBodyContent.tsx
@@ -10,42 +10,39 @@ const BalanceBodyContent = () => {
   const formatAmount = useFormatAmount();
   return (
     <AppearGrow trigger direction="x">
-      {state.balanceHistory.map((item, index) => {
-        return (
-          <AppearFadeIn key={index} direction="bottom" delay={0.1 * index}>
-            <div
-              className={cn(
-                "p-4 flex justify-between group",
-                glassmorphism({ hover: true }),
-              )}
-            >
-              <div className="flex items-center gap-4 font-medium group-hover:text-gray-300">
-                <div>
-                  {item.user.name} ({item.user.npm})<div>{item.note}</div>
-                </div>
-              </div>
-              <div className="flex flex-col gap-1 text-end">
-                <div
-                  className={cn(
-                    "font-medium ",
-                    item.amount > 0
-                      ? "text-green-500 group-hover:text-green-600"
-                      : item.amount < 0
-                        ? "text-red-500 group-hover:text-red-600"
-                        : "text-gray-300",
-                  )}
-                >
-                  {formatAmount(item.amount)}
-                </div>
-                <div className="group-hover:text-gray-300">
-                  {item.created_at}
-                </div>
-              </div>
+      {state.balanceHistory.map((item, index) => (
+        <AppearFadeIn key={index} direction="bottom" delay={0.2 * index}>
+          <div
+            className={cn(
+              "p-4 flex justify-between group",
+              glassmorphism({ hover: true }),
+            )}
+          >
+            <div className="flex flex-col">
+              <span>
+                {item.user.name} ({item.user.npm})
+              </span>
+              <span>{item.note}</span>
             </div>
-            {index !== state.balanceHistory.length - 1 && <hr />}
-          </AppearFadeIn>
-        );
-      })}
+            <div className="flex flex-col text-right">
+              <span
+                className={cn(
+                  "font-medium ",
+                  item.amount > 0
+                    ? "text-green-500 group-hover:text-green-600"
+                    : item.amount < 0
+                      ? "text-red-500 group-hover:text-red-600"
+                      : "text-gray-300",
+                )}
+              >
+                {formatAmount(item.amount)}
+              </span>
+              <span>{item.created_at}</span>
+            </div>
+          </div>
+          {index !== state.balanceHistory.length - 1 && <hr />}
+        </AppearFadeIn>
+      ))}
     </AppearGrow>
   );
 };
